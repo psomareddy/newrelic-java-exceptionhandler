6945b166fa3d271654d9bd8cb512c0b6af00487e
Prakash Reddy <preddy@newrelic.com>
2021-06-03 17:37:34 -0400

    update class

--- src/main/java/com/newrelic/labs/exception/ExceptionHandler.java
+++ src/main/java/com/newrelic/labs/exception/ExceptionHandler.java
@@ -1,22 +1,18 @@
 package com.newrelic.labs.exception;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
-@Weave(originalName = "javax.servlet.http.HttpServlet", type = MatchType.BaseClass)
-//@Weave(originalName = "com.guidewire.pl.system.integration.plugins.PluginInvocationHandler", type = MatchType.BaseClass)
+@Weave(originalName = "com.guidewire.pl.system.integration.plugins.PluginInvocationHandler", type = MatchType.BaseClass)
 public class ExceptionHandler {
 	
 	@Trace(dispatcher = true)
-	protected void service(HttpServletRequest request, HttpServletResponse response) {
+	public java.lang.Object invoke(java.lang.Object proxy, java.lang.reflect.Method method, java.lang.Object[] args) {
 		try {
-			Weaver.callOriginal();
+			return Weaver.callOriginal();
 		} catch (Throwable t) {
 			NewRelic.noticeError(t);
 			throw t;
